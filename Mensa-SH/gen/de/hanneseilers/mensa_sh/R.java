6da695e6cb16980dfd220d45e04650e5f20d5e9d
Hannes Eilers <hanneseilers@gmx.de>
2013-09-08 13:16:03 +0200

    Fixed gui errors.

--- Mensa-SH/gen/de/hanneseilers/mensa_sh/R.java
+++ Mensa-SH/gen/de/hanneseilers/mensa_sh/R.java
@@ -56,8 +56,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int info=0x7f090000;
-        public static final int menue=0x7f090001;
+        public static final int menue=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f06000d;
--- Mensa-SH/src/de/hanneseilers/mensa_sh/activities/ActivityInfo.java
+++ Mensa-SH/src/de/hanneseilers/mensa_sh/activities/ActivityInfo.java
@@ -3,7 +3,6 @@ package de.hanneseilers.mensa_sh.activities;
 import de.hanneseilers.mensa_sh.R;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class ActivityInfo extends Activity {
 
@@ -11,13 +10,6 @@ public class ActivityInfo extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.info, menu);
-		return true;
 	}
 
 }
