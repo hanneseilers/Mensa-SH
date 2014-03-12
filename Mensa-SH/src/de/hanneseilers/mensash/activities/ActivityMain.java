ed769f1a9185c6fe15bf41675fe4d92c3ed4d8af
Johan <johan.forstner@gmail.com>
2014-03-12 13:08:05 +0100

    orange Action Bar

--- Mensa-SH/src/de/hanneseilers/mensash/activities/ActivityMain.java
+++ Mensa-SH/src/de/hanneseilers/mensash/activities/ActivityMain.java
@@ -196,6 +196,7 @@ public class ActivityMain extends Activity implements MenuFragment.Callback {
         
         // Bind the widget to the adapter
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setIndicatorColorResource(R.color.orange);
         tabs.setViewPager(mViewPager);
 		
 		for (MenuFragment frag : menuFragments) {
